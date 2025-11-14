55bd6ec1e6fc07b5c1019e39f9e39910e1db9a44
SolomiiaIvanovska <ivanovska.s@ufl.edu>
2025-11-13 21:52:59 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,21 +1,64 @@
 #include "WelcomeWindow.hpp"
-
 #include "GameWindow.h"
-#include <SFML/Graphics.hpp>
 #include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
 
-int main() {
+// Read config.cfg
+// bool loadConfig(int& rows, int& cols, int& mines)
+// {
+//     rows = 16;
+//     cols = 25;
+//     mines = 50;
+//
+//     std::ifstream in("config.cfg");
+//     if (!in.is_open()) {
+//         std::cerr << "Could not open config.cfg, using defaults.\n";
+//         return false;
+//     }
+//
+//     std::stringstream buffer;
+//     buffer << in.rdbuf();
+//     std::string s = buffer.str();
+//     for (char& ch : s) {
+//         if (ch == ',' || ch == '\n' || ch == '\r' || ch == '\t')
+//             ch = ' ';
+//     }
+//
+//     std::stringstream ss(s);
+//     int c, r, m;
+//     if (!(ss >> c >> r >> m)) {
+//         std::cerr << "Error parsing config.cfg, using defaults.\n";
+//         return false;
+//     }
+//
+//     cols = c;
+//     rows = r;
+//     mines = m;
+//     return true;
+// }
+
+int main()
+{
+    int rows, cols, mines;
+    // loadConfig(rows, cols, mines);
+
+    // Show welcome window
     WelcomeWindow welcome;
 
-    if (welcome.run()) {
-        std::string name = welcome.getPlayerName();
-        std::cout << "Player: " << name << "\n";
+    if (!welcome.run())
+        return 0;
 
-        int rows = 16;
-        int cols = 25;
-        GameWindow game(rows, cols);
-        game.run();
-    }
+    std::string playerName = welcome.getPlayerName();
+    std::cout << "Player: " << playerName << "\n";
+    // std::cout << "Config => " << cols << "x" << rows << " mines: " << mines << "\n";
+
+    // GameWindow constructor DOES NOT need rows/cols/mines
+    GameWindow game(playerName);
+    game.run();
 
     return 0;
 }
+
+
